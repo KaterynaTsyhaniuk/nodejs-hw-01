53885fafc563bab706cf3bbc7bf9e9ab49e7a835
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2025-01-07 23:21:19 +0200

    add 2step hw1nodeJs

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts);
+    await fs.writeFile(PATH_DB, data, 'utf8');
+    console.log('The data was successfully written to the file.');
+  } catch (error) {
+    console.error('Error writing to file:', error);
+  }
+};
 
-export const writeContacts = async (updatedContacts) => {};
+writeContacts();
