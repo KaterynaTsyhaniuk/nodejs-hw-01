c7fa3995f1daf47944451295bff14c8cad5cdcf5
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2025-01-09 00:03:16 +0200

    add 4 step to hw nodejs

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,23 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    const myData = await readContacts();
+    const contacts = myData ? JSON.parse(myData) : [];
+
+    if (!Array.isArray(contacts)) {
+      throw new Error('Invalid data format in db.json');
+    }
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+
+    await writeContacts(contacts);
+    console.log(`New contact successfully added`);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 addOneContact();
