8a80d4e996355e5ebfe738a7d357d08a6ec09a5c
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2025-01-09 00:34:43 +0200

    add step 5 hw nodejs

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,18 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const myData = await readContacts();
+    const contacts = myData ? JSON.parse(myData) : [];
+
+    if (!Array.isArray(contacts)) {
+      throw new Error('Invalid data format in db.json');
+    }
+    return contacts;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
 
 console.log(await getAllContacts());
