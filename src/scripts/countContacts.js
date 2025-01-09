7db9198f6761cee677a0b5e6ceb842fd5cf78212
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2025-01-09 22:59:26 +0200

    add 6 7 8 step hw nodejs

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,13 @@
-export const countContacts = async () => {};
+import { getAllContacts } from './getAllContacts.js';
+
+export const countContacts = async () => {
+  try {
+    const contacts = await getAllContacts();
+    return contacts.length;
+  } catch (error) {
+    console.error(error);
+    return 0;
+  }
+};
 
 console.log(await countContacts());
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,22 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const data = await readContacts();
+    const contacts = data ? JSON.parse(data) : [];
+
+    if (contacts.length === 0) {
+      return;
+    }
+
+    contacts.pop();
+
+    await writeContacts(contacts);
+    console.log('We remove last contact!');
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 removeLastContact();
